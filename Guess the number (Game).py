be80ffa8124054d1c9af68595562f719eabd1781
Maksim Nikitin <nimacogroup@gmail.com>
2019-11-10 03:47:09 +0500

    Add an intuitive dialogue (hot, warm, cold)

--- Guess the number (Game).py
+++ Guess the number (Game).py
@@ -1,13 +1,8 @@
 import random
 
-'''
-
-'''
-
 print('Игра "Угадай число"')
 cnt = 0  # счетчик (сколько было попыток)
 t = list()  # записывает все числа, которые вводились
-
 
 n = random.randint(0, 100)  # загаданное число
 print(n)
@@ -18,11 +13,13 @@ def get_int(prompt):
         try:
             value = int(input(prompt))
             global cnt, t
-            cnt += 1
+            # cnt += 1
         except ValueError:
             print("Некоректная запись. Нужно написать ЦЕЛОЕ ЧИСЛО!")
-            cnt += 1
+            # cnt += 1
             continue
+        finally:
+            cnt += 1
         if value in t:
             print('Вы уже вводили это число!')
             continue
@@ -39,14 +36,36 @@ def get_int(prompt):
     return value
 
 
+def set_min(m):
+    raznica = n - m
+    if raznica > 21:
+        return print('Холодно! Загаданное число БОЛЬШЕ.')
+    if 10 < raznica <= 20:
+        t.append(m)
+        return print('Тепло! Загаданное число БОЛЬШЕ.')
+    elif 1 < raznica <= 10:
+        t.append(m)
+        return print('Горячо! Загаданное число БОЛЬШЕ.')
+
+
+def set_max(m):
+    raznica = m - n
+    if raznica > 21:
+        return print('Холодно! Загаданное число МЕНЬШЕ.')
+    if 10 < raznica <= 20:
+        t.append(m)
+        return print('Тепло! Загаданное число МЕНЬШЕ.')
+    elif 1 < raznica <= 10:
+        t.append(m)
+        return print('Горячо! Загаданное число МЕНЬШЕ.')
+
+
 m = get_int('')  # вводимое число
 while m != n:
     if m < n:
-        print('Неверно! Загаданное число БОЛЬШЕ.')
-        t.append(m)
+        set_min(m)
         m = get_int('')
     elif m > n:
-        print('Неверно! Загаданное число МЕНЬШЕ.')
-        t.append(m)
+        set_max(m)
         m = get_int('')
-print('Поздравляем! Вы угадали c {} попытки' .format(cnt))
+print('Поздравляем! Вы угадали c {} попытки'.format(cnt))
